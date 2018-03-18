9241756cf79f16e1ced12aba6fc5d24ff3852cf7
David Percy <davetp425@gmail.com>
2018-03-17 20:25:01 -0400

    more advent

--- slapdash/src/Prim.hs
+++ slapdash/src/Prim.hs
@@ -69,6 +69,8 @@ primRules = alts [
   binop @Int @String "drop" drop,
 
   unop "isString" isString,
+  unop "parseInt" (read @Integer),
+  unop "showInt" (show @Integer),
   
   binop @Expr "equal" (==)
   ]
